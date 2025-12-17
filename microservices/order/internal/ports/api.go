b6e0709786c3ce4811cb692b4a40e4ef5c377d5f
LuizFernando <128930132+Luiz-Gomess@users.noreply.github.com>
2025-12-17 11:03:54 -0300

    Delete microservices/order directory

--- microservices/order/internal/ports/api.go
+++
@@ -1,7 +0,0 @@
-package ports
-
-import "github.com/Luiz-Gomess/microservices/order/internal/application/core/domain"
-
-type APIPort interface {
-	PlaceOrder(order domain.Order) (domain.Order, error)
-}
--- microservices/order/internal/ports/db.go
+++
@@ -1,8 +0,0 @@
-package ports
-
-import "github.com/Luiz-Gomess/microservices/order/internal/application/core/domain"
-
-type DBPort interface {
-	Get(id string) (domain.Order, error)
-	Save(*domain.Order) error
-}
